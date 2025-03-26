89cb3723dc9af673e5586c4558b706f608539783
dudatntd <2005nguyendat123@gmail.com>
2025-03-26 21:40:05 +0700

    buoi8

---
+++ buoi8/product_controller.js
@@ -0,0 +1,16 @@
+const categories = [
+    { id: 1, name: "Điện thoại", description: "Các dòng điện thoại thông minh và phụ kiện" },
+    { id: 2, name: "Laptop", description: "Laptop các hãng khác nhau" },
+    { id: 3, name: "Phụ kiện", description: "Tai nghe, sạc, cáp,..." }
+];
+
+const Product = require("../model/product");
+
+class ProductController {
+    static async index(req, res) {
+        const products = await Product.find();
+        res.render("product", { products });
+    }
+}
+
+module.exports = ProductController;
